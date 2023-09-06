aeff3b9b5f959590e42306361549844eb6300b7c
Arthur Santos <arthur.pentester@gmail.com>
2023-09-05 23:30:37 -0300

    3th commit

--- async_wait_overview.py
+++ async_wait_overview.py
@@ -3,5 +3,11 @@ import asyncio
 async def sum(a,b):
     return a+b
 
-corrotina =sum(3,2)
+async def print_sum(a,b):
+    result = await sum(a,b)
+    print(f'resultado igual a {result}')
 
+
+# EVENT LOOP
+event_loop = asyncio.new_event_loop()
+event_loop.run_until_complete(print_sum(2,6))
